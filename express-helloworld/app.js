9dc7b8b130b9b993560fcdc732ae557ad983700d
Student User <student@workstation.lab.example.com>
2025-04-28 12:30:51 -0400

    convert app.js to HTTP server so container stays up

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,1 +1,16 @@
-console.log("Hello Wordld!\n");
+const express = require('express');
+const app = express();
+
+// Openshift port or 8080 as a fallback
+const port = process.env.PORT || 8080;
+
+// Define a single “/” route returning “Hello World!”
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Listening on 0.0.0.0 for OpenShift to route to
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
+});
+
